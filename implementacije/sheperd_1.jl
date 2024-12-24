c0ec6b63c3a355b7e97f952647d3af33344049a2
amujalo1 <140057750+amujalo1@users.noreply.github.com>
2024-12-24 10:04:08 +0100

    Update sheperd_1.jl

--- implementacije/sheperd_1.jl
+++ implementacije/sheperd_1.jl
@@ -1,17 +1,14 @@
 using Plots
-plotly()  # Set the backend to PlotlyJS
+plotly() 
 
-# Define the function f(x, y)
 function f(x, y)
     return sin((x - y) / 2) * cos(sqrt(x^2 + y^2) / 2)
 end
 
-# Function to compute Euclidean distance
 function euclidean_distance(x1, y1, x2, y2)
     sqrt((x1 - x2)^2 + (y1 - y2)^2)
 end
 
-# Shepard's interpolation function
 function shepard_interpolation(x, y, x_known, y_known, f_known, p=2)
     N = length(f_known)
     numerator = 0.0
@@ -40,14 +37,11 @@ y_known = [-3.0, -2.75, -2.5, -2.25, -2.0, -1.75, -1.5, -1.25, -1.0, -0.75, -0.5
 
 f_known = [f(x_known[i], y_known[i]) for i in 1:length(x_known)]
 
-# Define the range of points for interpolation
 x_grid = -3:0.1:3
 y_grid = -3:0.1:3
 
-# Compute interpolated values for each point in the grid
 X = [xi for xi in x_grid, yi in y_grid]
 Y = [yi for xi in x_grid, yi in y_grid]
 Z = [shepard_interpolation(xi, yi, x_known, y_known, f_known) for xi in x_grid, yi in y_grid]
 
-# Plot the 3D surface
 surface(X, Y, Z, xlabel="x", ylabel="y", zlabel="Interpolated f(x, y)", title="3D Surface Plot of Shepard Interpolation")
